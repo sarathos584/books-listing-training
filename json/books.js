8f35115ef70c021aeecdde94b4b92dc4ef059e5c
sarath <sarath.limenzy@gmail.com>
2024-04-09 15:21:13 +0530

    add structure and json data

---
+++ json/books.js
@@ -0,0 +1,52 @@
+const books = [
+    {
+        name: 'Francis Itty Cora',
+        Author: 'T. D. Ramakrishnan',
+        genre: 'Thriller, mystery',
+        star_rating: 5,
+        published: '2009 August',
+        price: 499,
+        language: 'Malayalam',
+        image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1468174203l/11295292.jpg'
+    },
+    {
+        name: 'Nile Diary',
+        Author: 'S.K. Pottekkatt',
+        genre: 'Travelogue',
+        star_rating: 5,
+        published: '1954 January',
+        price: 80,
+        language: 'Malayalam',
+        image: 'https://m.media-amazon.com/images/I/7181zQZgS9L._AC_UF1000,1000_QL80_.jpg'
+    },
+    {
+        name: 'Atomic Habits',
+        Author: 'James Clear',
+        genre: 'Non-fiction',
+        star_rating: 4,
+        published: '2018 October',
+        price: 300,
+        language: 'English',
+        image: 'https://m.media-amazon.com/images/I/81Ls+SBCLiL._AC_UF1000,1000_QL80_.jpg'
+    },
+    {
+        name: 'Aadujeevitham',
+        Author: 'Benyamin',
+        genre: 'Novel',
+        star_rating: 5,
+        published: '2008 July',
+        price: 300,
+        language: 'Malayalam',
+        image: 'https://upload.wikimedia.org/wikipedia/en/b/b5/Adujivitam.jpg'
+    },
+   {
+        name: 'Totto Chan',
+        Author: 'Tetsuko Kuroyanagi',
+        genre: 'Fictional Autobiography',
+        star_rating: 5,
+        published: '1981 January',
+        price: 120,
+        language: 'Japanese',
+        image: 'https://m.media-amazon.com/images/I/61lF7e2+liL._AC_UF1000,1000_QL80_.jpg'
+    }
+]
---
+++ json/reviews.js
@@ -0,0 +1,38 @@
+const reviews = [
+    {
+        _id: 1,
+        user: 'John Doe',
+        rating:4,
+        book_name: 'Francis Itty Cora',
+        review: 'I am utterly captivated by "Francis Ittikkora"! Hats off to the incredible imagination of the author. The way you have intertwined history and fiction is nothing short of masterful. '
+    },
+    {
+      _id: 2,
+      user: 'Shreeya',
+      rating:3,
+      book_name: 'Nile Diary',
+      review: 'S,K Pottedkad a travelogue writer of Malayalam gives his experience in travelling through places where Nile floats . His narration has made us as if we are travelling along with him.'
+    },
+    {
+      _id: 3,
+      user: 'Samuel Hare',
+      rating:4,
+      book_name: 'Atomic Habits',
+      review: 'A remarkable book that effectively conveys how small (atomic) changes to ones daily routine can yield unprecedented results in the long run.'
+    },
+    {
+      _id: 4,
+      user: 'Arun Divakar',
+      rating:5,
+      book_name: 'Aadujeevitham',
+      review: "Aadujeevitham is not a cliche story of gulf malayalees..it is about the pain and sufferings of protagonist najeeb and also of hope and faith in God ..really heart touching story..a must read for all those who complains about their silly problems and belittle other people' sufferings..The style of narration was really good too :)"
+    },
+    {
+      _id: 5,
+      user: 'Jiahe Liu',
+      rating:4,
+      book_name: 'Totto Chan',
+      review: "I love this book so much that I can't stop reading it over and over again! Truly worth reading. Who can believe that this lovely, beautiful story happened during the second world war?! I hope every teacher can learn from Mr Sosaku Kobayashi and let kids' imagination fly"
+      
+    }
+  ]
